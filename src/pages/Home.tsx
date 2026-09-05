7b850fdc26ae3af768b8bbd69b5353956bc1362a
Githubguy7 <161179740+Githubguy7@users.noreply.github.com>
2026-09-04 22:55:09 -0700

    Fix unused Link import in homepage build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo, useState } from 'react'
-import { Link } from 'react-router-dom'
 import { Headphones, Music2, Play, Search } from 'lucide-react'
 import { SongCard } from '@/components/SongCard'
 import { useAudioPlayer } from '@/contexts/AudioPlayerContext'
